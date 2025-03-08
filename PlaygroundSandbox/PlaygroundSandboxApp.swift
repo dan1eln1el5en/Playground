7cd21f8f903e1162105c0bf7f4365a8755ab51c8
Daniel Nielsen <daniel@aplacebetweenhere.net>
2025-03-08 21:02:07 +0100

    Initial Commit

---
+++ PlaygroundSandbox/PlaygroundSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundSandboxApp.swift
+//  PlaygroundSandbox
+//
+//  Created by Daniel Nielsen on 08/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
